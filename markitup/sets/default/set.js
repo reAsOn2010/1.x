4b35456f63344590f8fad89f0a998aea4b7b47ff
Leon Zhou <the.reason.sake@gmail.com>
2012-07-08 10:49:39 +0800

    make the html convertion support headings(h1,h2...h6) bootstrap supports these headings

--- markitup/sets/default/set.js
+++ markitup/sets/default/set.js
@@ -14,6 +14,13 @@ var mySettings = {
 	onCtrlEnter:  	{keepDefault:false, openWith:'\n<p>', closeWith:'</p>'},
 	onTab:    		{keepDefault:false, replaceWith:'    '},
 	markupSet:  [ 	
+		{name:'H1', key:'1', openWith:'<h1>', closeWith:'</h1>'  },
+		{name:'H2', key:'2', openWith:'<h2>', closeWith:'</h2>'  },
+		{name:'H3', key:'3', openWith:'<h3>', closeWith:'</h3>'  },
+		{name:'H4', key:'4', openWith:'<h4>', closeWith:'</h4>'  },
+		{name:'H5', key:'5', openWith:'<h5>', closeWith:'</h5>'  },
+		{name:'H6', key:'6', openWith:'<h6>', closeWith:'</h6>'  },
+		{separator:'---------------' },
 		{name:'Bold', key:'B', openWith:'(!(<strong>|!|<b>)!)', closeWith:'(!(</strong>|!|</b>)!)' },
 		{name:'Italic', key:'I', openWith:'(!(<em>|!|<i>)!)', closeWith:'(!(</em>|!|</i>)!)'  },
 		{name:'Stroke through', key:'S', openWith:'<del>', closeWith:'</del>' },
